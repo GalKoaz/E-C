3d798b419fdb4e4eddcc4ca2c07f42672be42b1e
Gal Koaz <76553647+GalKoaz@users.noreply.github.com>
2023-09-25 23:43:36 +0300

    Upload and Fix the function of Files

---
+++ server.h
@@ -0,0 +1,10 @@
+
+
+#ifndef UNTITLED9_MAIN_H
+#define UNTITLED9_MAIN_H
+
+void write_file(int client_socket);
+void download_file(int client_socket);
+void list_files(int client_socket);
+
+#endif //UNTITLED9_MAIN_H
