bd6cc52b00eca8a3d8675df8a13678e2205ca92b
Gal Koaz <koazgal@gmail.com>
2023-09-27 20:05:58 +0300

    Fix problems

--- functions.c
+++ functions.c
@@ -2,16 +2,36 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <dirent.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
 
 #include "server.h"
 
 #define SIZE 1024
+#define MAX_FILENAME_LENGTH 256
 
+
+uint64_t ntohll(uint64_t value) {
+    static const int num = 42;
+    if (*(char *)&num == 42) {
+        uint64_t result = 0;
+        for (int i = 0; i < 8; i++) {
+            result = (result << 8) | ((value >> (i * 8)) & 0xFF);
+        }
+        return result;
+    } else {
+        return value;
+    }
+}
 
 void write_file(int client_socket) {
     char buffer[SIZE];
     char filename[SIZE];
     ssize_t bytes_received;
+    off_t file_size;
+
     FILE *file;
 
     bytes_received = recv(client_socket, filename, sizeof(filename), 0);
@@ -21,12 +41,15 @@ void write_file(int client_socket) {
     }
     filename[bytes_received] = '\0';
 
-    off_t file_size;
-    bytes_received = recv(client_socket, &file_size, sizeof(off_t), 0);
+    uint64_t network_file_size;
+    bytes_received = recv(client_socket, &network_file_size, sizeof(network_file_size), 0);
     if (bytes_received < 0) {
         perror("Error receiving file size");
         return;
     }
+
+
+    file_size = ntohll(network_file_size);
 
     file = fopen(filename, "wb");
     if (file == NULL) {
@@ -42,15 +65,15 @@ void write_file(int client_socket) {
         }
         fwrite(buffer, 1, bytes_received, file);
         total_received += bytes_received;
-
-        if (strstr(buffer, "EOF") != NULL) {
-            break;
-        }
     }
 
     fclose(file);
     printf("File '%s' received and saved successfully.\n", filename);
 }
+
+
+
+
 
 void download_file(int client_socket) {
     char filename[SIZE];
@@ -83,29 +106,7 @@ void download_file(int client_socket) {
     printf("File '%s' sent successfully.\n", filename);
 }
 
-//void list_files(int client_socket) {
-//    DIR *dir;
-//    struct dirent *entry;
-//
-//    dir = opendir("./");
-//    if (dir == NULL) {
-//        perror("Error opening directory");
-//        return;
-//    }
-//
-//    char file_list[SIZE] = "";
-//
-//    while ((entry = readdir(dir)) != NULL) {
-//        strcat(file_list, entry->d_name);
-//        strcat(file_list, "\n");
-//    }
-//
-//    closedir(dir);
-//
-//
-//    send(client_socket, file_list, strlen(file_list), 0);
-//    printf("File list sent to the client.\n");
-//}
+
 void list_files(int client_socket) {
     DIR *dir;
     struct dirent *entry;
@@ -120,7 +121,7 @@ void list_files(int client_socket) {
     size_t file_list_length = 0;
 
     while ((entry = readdir(dir)) != NULL) {
-        // Append file name and newline to the file list
+
         strcat(file_list, entry->d_name);
         strcat(file_list, "\n");
         file_list_length += strlen(entry->d_name) + 1;
@@ -128,16 +129,13 @@ void list_files(int client_socket) {
 
     closedir(dir);
 
-    // Send the file list length to the client first
-    char length_buffer[16];  // Assuming the length won't exceed 15 digits
+    char length_buffer[16];
     snprintf(length_buffer, sizeof(length_buffer), "%zu", file_list_length);
     send(client_socket, length_buffer, strlen(length_buffer), 0);
 
-    // Wait for acknowledgment from the client (optional)
     char ack[3];
     recv(client_socket, ack, sizeof(ack), 0);
 
-    // Send the file list to the client
     send(client_socket, file_list, file_list_length, 0);
     printf("File list sent to the client.\n");
 }
